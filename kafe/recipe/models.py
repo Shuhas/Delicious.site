9451b8eabdf8e855d9a53712ecbf1999f1f681fb
Shuhas <as.shuhas@gmail.com>
2022-01-27 19:48:06 +0500

    Category va Recipe classlarini yaratdim.

--- kafe/recipe/models.py
+++ kafe/recipe/models.py
@@ -1,6 +1,35 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
+
+def default_rate():
+    return (
+        (1, "⭐"),
+        (2, "⭐⭐"),
+        (3, "⭐⭐⭐"),
+        (4, "⭐⭐⭐⭐"),
+        (5, "⭐⭐⭐⭐⭐")
+    )
 
 class Category(models.Model):
     content = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128, blank=True, null=True)
+
+    def __str__(self):
+        return self.content
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.content)
+        super(Category, self).save(*args, **kwargs)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=256)
+    steps = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    prep = models.IntegerField()
+    cook = models.IntegerField()
+    yields = models.IntegerField()
+    rate = models.IntegerField(choices=default_rate)
+    img = models.ImageField()
+    ctg = models.ForeignKey(Category, on_delete=models.CASCADE)
